27059e0c13888f98ed16432629da22489e56a160
Mbanefo Emmanuel <emmanuelifeh28@gmail.com>
2026-04-29 16:44:21 +0100

    Update depPage.js

--- depPage.js
+++ depPage.js
@@ -1,1 +1,4 @@
-
+const AmountDeposited = document.querySelector('.dep-amount')
+const firstInputedPin = document.querySelector('.inp-pin')
+const secondInputerPin = document.querySelector('.retype')
+console.log(firstInputedPin.value === secondInputerPin.value)
