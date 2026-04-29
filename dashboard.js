0a041852cbee65e2f049b2615fa90308954e5a25
Mbanefo Emmanuel <emmanuelifeh28@gmail.com>
2026-04-29 16:44:55 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,16 @@
+const userName = document.querySelector('.user-welcome');
+const balance = document.querySelector('.balance');
+const depositBtn = document.querySelector('.dep-button');
+const transferBtn = document.querySelector('.transfer-btn');
+const withdrawBtn = document.querySelector('.withdraw-btn');
+const historyBtn = document.querySelector('.history-btn')
 
+const welcomeMsg = localStorage.getItem('userName')   
+
+userName.textContent = `Welcome, ${welcomeMsg}`    
+    
+depositBtn.addEventListener('click', (e) => {
+      setTimeout(() => {
+        window.location.href = 'depPage.html'
+      }, 700)
+    })
