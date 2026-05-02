873ee15017678cdbc348254f4f44822832bd268c
Mbanefo Emmanuel <emmanuelifeh28@gmail.com>
2026-05-02 12:59:09 +0100

    Refactor deposit form submission handling

--- depPage.js
+++ depPage.js
@@ -1,21 +1,42 @@
+const depositForm = document.querySelector('.deposit-form')
 const amountDeposited = document.querySelector('.dep-amount')
 const firstInputedPin = document.querySelector('.inp-pin')
 const secondInputerPin = document.querySelector('.retype');
 const depBtn = document.querySelector('.final-depBtn')
 console.log(firstInputedPin.value === secondInputerPin.value)
 
-depBtn.addEventListener('click', (e) => {
+depositForm?.addEventListener('submit', (e) => {
   e.preventDefault();
-  let trimmedAmt = amountDeposited.value.trim();
+  let trimmedAmt = Number(amountDeposited.value.trim())
   let trimmedPin = firstInputedPin.value.trim();
   let trimmedPin2 = secondInputerPin.value.trim();
   
   if (!trimmedAmt || !trimmedPin || !trimmedPin2) {
-    alert('Please characters must be filled!')
-    return
+    return alert('Please characters must be filled!')
   }
   
-  if (trimmedPin === trimmedPin2) {
+  if (isNaN(trimmedAmt || trimmedAmt < 0)) {
+    return alert('Enter a valid amount')
+  }
+  
+  if (trimmedPin !== trimmedPin2) {
+    return alert("PIN doesn't match")
+  }
+  
+  if (trimmedPin < 4) {
+    return alert("PIN must be 4 length")
+  }
+  
+  const balance = Number(localStorage.getItem('amount') || 0)
+  
+  const newBalance = balance + trimmedAmt
+  localStorage.setItem('amount', newBalance)
+  
+  alert(`Deposited ${trimmedAmt.toLocaleString()} successfully`)
+  setTimeout(() => {
+    window.location.href = 'dashboard.html'
+  }, 800)
+  /* if (trimmedPin === trimmedPin2) {
     alert('Deposited successfully')
     setTimeout(() => {
     window.location.href = 'dashboard.html'
@@ -25,6 +46,6 @@ depBtn.addEventListener('click', (e) => {
   }
   
   localStorage.setItem('amount', amountDeposited.value)
-  
+  */
   
 })
